47d169f9a79cccfdfada0722fc0e3b3d2d86deb9
Vladyslav Usenko <vu4848@gmail.com>
2023-08-20 12:30:28 +0300

    refactor config init

--- config/config.go
+++ config/config.go
@@ -1,25 +1,15 @@
 package config
 
 import (
-	"crypto/rand"
-	_ "embed"
-	"encoding/json"
-	"errors"
 	"os"
-	"sync"
 
-	"github.com/xeipuuv/gojsonschema"
-	"vladusenko.io/home-torrent/defaults"
+	"github.com/knadh/koanf/parsers/yaml"
+	"github.com/knadh/koanf/providers/file"
+	"github.com/knadh/koanf/v2"
 )
 
 type LoggingConfig struct {
-	LogLevel   string `json:"log_level"`
-	Console    bool   `json:"console"`
-	Directory  string `json:"directory"`
-	Filename   string `json:"file_name"`
-	MaxSize    int    `json:"max_size"`
-	MaxBackups int    `json:"max_backups"`
-	MaxAge     int    `json:"max_age"`
+	LogLevel string `koanf:"log_level"`
 }
 
 type TorrentConfig struct {
@@ -28,87 +18,26 @@ type TorrentConfig struct {
 }
 
 type Config struct {
-	HttpPort int            `json:"http_port"`
-	Logging  *LoggingConfig `json:"logging"`
-	Torrent  *TorrentConfig `json:"torrent"`
+	HttpPort int            `koanf:"http_port"`
+	Logging  *LoggingConfig `koanf:"logging"`
+	Torrent  *TorrentConfig `koanf:"torrent"`
 }
 
-//go:embed schema.json
-var configSchema string
-
-var config *Config = nil
-var schema *gojsonschema.Schema = nil
-var once *sync.Once = new(sync.Once)
-
-// NOTE for unit tests only
-func Reset() {
-	once = new(sync.Once)
-}
-
-func LoadConfig(path string) (*Config, error) {
-	var err error
-	once.Do(func() {
-		schemaLoader := gojsonschema.NewStringLoader(configSchema)
-		if schema, err = gojsonschema.NewSchema(schemaLoader); err != nil {
-			panic(err)
-		}
-		config, err = parseAndValidateConfig(path)
-	})
-
-	return config, err
-}
-
-func GetConfig() (*Config, error) {
-	if config == nil {
-		return nil, errors.New("config has not been parsed yet")
+func InitConfig() (error, *Config) {
+	path := os.Getenv(`CONFIG_PATH`)
+	if path == "" {
+		path = "./torrent_config.yaml"
 	}
-	return config, nil
-}
-
-func parseAndValidateConfig(path string) (*Config, error) {
-	var raw []byte
-	var validationResult *gojsonschema.Result
-	var peerId [20]byte
-	var err error
-
-	if _, err := rand.Read(peerId[:]); err != nil {
-		return nil, err
+	k := koanf.New(".")
+	if err := k.Load(file.Provider(path), yaml.Parser()); err != nil {
+		return err, nil
 	}
 
-	if raw, err = os.ReadFile(path); err != nil {
-		return nil, err
+	config := &Config{}
+
+	if err := k.UnmarshalWithConf(".", config, koanf.UnmarshalConf{Tag: "koanf"}); err != nil {
+		return err, nil
 	}
 
-	loader := gojsonschema.NewBytesLoader(raw)
-
-	if validationResult, err = schema.Validate(loader); err != nil {
-		return nil, err
-	}
-
-	if !validationResult.Valid() {
-		return nil, errors.New("config file is invalid")
-	}
-
-	config := &Config{
-		HttpPort: defaults.DEFAULT_HTTP_PORT,
-		Logging: &LoggingConfig{
-			LogLevel:   "info",
-			Console:    true,
-			Directory:  "./logs",
-			Filename:   "ht.log",
-			MaxSize:    10,
-			MaxBackups: 25,
-			MaxAge:     30,
-		},
-		Torrent: &TorrentConfig{
-			PeerId:   peerId,
-			PeerPort: defaults.DEFAULT_TORRENT_PEER_PORT,
-		},
-	}
-
-	if err = json.Unmarshal(raw, config); err != nil {
-		return nil, err
-	}
-
-	return config, nil
+	return nil, config
 }
--- go.mod
+++ go.mod
@@ -1,27 +1,39 @@
 module github.com/home-torrent
 
-go 1.16
+go 1.20
+
+require (
+	github.com/gin-gonic/gin v1.7.1
+	github.com/jackpal/bencode-go v1.0.0
+	github.com/knadh/koanf/v2 v2.0.1
+	github.com/onsi/ginkgo v1.16.2
+	github.com/onsi/gomega v1.12.0
+	gopkg.in/yaml.v2 v2.4.0
+)
 
 require (
 	github.com/armon/consul-api v0.0.0-20180202201655-eb2c6b5be1b6 // indirect
 	github.com/coreos/etcd v3.3.10+incompatible // indirect
 	github.com/coreos/go-etcd v2.0.0+incompatible // indirect
-	github.com/gin-gonic/gin v1.7.1
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/go-playground/validator/v10 v10.6.1 // indirect
-	github.com/jackpal/bencode-go v1.0.0
 	github.com/json-iterator/go v1.1.11 // indirect
+	github.com/knadh/koanf/maps v0.1.1 // indirect
+	github.com/knadh/koanf/parsers/yaml v0.1.0 // indirect
+	github.com/knadh/koanf/providers/file v0.1.0 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
-	github.com/onsi/ginkgo v1.16.2
-	github.com/onsi/gomega v1.12.0
 	github.com/rs/zerolog v1.22.0 // indirect
 	github.com/samber/do v1.6.0 // indirect
 	github.com/ugorji/go v1.2.6 // indirect
 	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
 	github.com/xordataexchange/crypt v0.0.3-0.20170626215501-b2862e3d0a77 // indirect
 	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
-	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015 // indirect
+	golang.org/x/sys v0.11.0 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
-	gopkg.in/yaml.v2 v2.4.0
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
